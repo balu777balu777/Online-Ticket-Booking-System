83c0b1abc63660fb33ba8e9a447cf1dc2c85a32b
Balu <baluatchutha777@gmail.com>
2025-08-01 18:47:24 +0530

    initial commit

---
+++ Online-Ticketing-System-main/js/main.js
@@ -0,0 +1,150 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+      
+      $('#form-submit .date').datepicker({
+      });
+
+      var owl = $("#owl-suiteroom");
+
+        owl.owlCarousel({
+          
+          pagination : true,
+          paginationNumbers: false,
+          autoPlay: 6000, //Set AutoPlay to 3 seconds
+          items : 1, //10 items above 1000px browser width
+          itemsDesktop : [1000,1], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,1], // betweem 900px and 601px
+          itemsTablet: [600,1], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+          
+      });
+
+
+      var owl = $("#owl-mostvisited");
+
+        owl.owlCarousel({
+          
+          pagination : true,
+          paginationNumbers: false,
+          autoPlay: 6000, //Set AutoPlay to 3 seconds
+          items : 4, //10 items above 1000px browser width
+          itemsDesktop : [1000,4], //5 items between 1000px and 901px
+          itemsDesktopSmall : [900,2], // betweem 900px and 601px
+          itemsTablet: [600,1], //2 items between 600 and 0
+          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+          
+      });
+
+
+        
+        $('.recommendedgroup > div').hide();
+        $('.recommendedgroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+          e.preventDefault();
+            var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('recommendedgroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
+      
+        })
+
+
+        $('.weathergroup > div').hide();
+        $('.weathergroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+          e.preventDefault();
+            var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('weathergroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
+      
+        })
+
+
+        $('.tabgroup > div').hide();
+        $('.tabgroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+          e.preventDefault();
+            var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
+      
+        })
+
+
+
+        $(".pop-button").click(function () {
+            $(".pop").fadeIn(300);
+            
+        });
+
+        $(".pop > span").click(function () {
+            $(".pop").fadeOut(300);
+        });
+
+
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".header").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in your css)
+               $(".header").removeClass("active");
+            }
+        });
+
+
+	/************** Mixitup (Filter Projects) *********************/
+    	$('.projects-holder').mixitup({
+            effects: ['fade','grayscale'],
+            easing: 'snap',
+            transitionSpeed: 400
+        });
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var orderButton = document.getElementById('form-submit');
+  if (orderButton) {
+    orderButton.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      // Collect field values
+      const from = document.querySelector('select[name="from"]');
+      const to = document.querySelector('select[name="to"]');
+      const departure = document.querySelector('input[name="departure"]');
+      const returnDate = document.querySelector('input[name="return"]');
+      const tripType = document.querySelector('input[name="trip"]:checked');
+
+      // Check if any required field is empty
+      if (!from.value || !to.value || !departure.value || !returnDate.value || !tripType) {
+        alert('Please fill in all the fields before booking.');
+        return;
+      }
+
+      // All good
+      alert('Tickets booked successfully and will be mailed to you');
+    });
+  }
+});        
+
+});
